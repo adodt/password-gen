ca97368045b69deafa5e55ff6b22b25042e2122b
Gator <allidodt@gmail.com>
2022-09-12 14:17:30 -0700

    confirming character parameters

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 //Arrays
-var nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var characters = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var arrNums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var arrCharacters = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var arrLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var arrUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 //Variables
 let confirmLength = "";
@@ -32,16 +32,45 @@ function generatePassword(){
 
     if(confirmLength <=8 || confirmLength >= 128){
         alert("Password must be between 9-127 characters.");
+        (prompt("How many charaters would you like in your password? Between 9-127."));
     } else (confirmLength >=9 || confirmLength <=128);
         alert(`Your password will have ${confirmLength} characters`);
+
+    let confirmCharacter = confirm("Would you like to include special characters?");
+    let confirmLowerCase = confirm("Would you like to include lowercase?");
+    let confirmUpperCase = confirm("Would you like to include uppercase?");
+    let confirmNumericCharacter= confirm("Would you like to include special characters?");
+
+    if(confirmCharacter === false && confirmLowerCase === false && confirmUpperCase === false && confirmNumericCharacter === false){
+        alert("Must choose at least one parameter")
+        let confirmCharacter = confirm("Would you like to include special characters?");
+    let confirmLowerCase = confirm("Would you like to include lowercase?");
+    let confirmUpperCase = confirm("Would you like to include uppercase?");
+    let confirmNumericCharacter= confirm("Would you like to include special characters?");
     }
 
+    let passwordCharacters = []
 
+    if (confirmCharacter){
+        passwordCharacters = passwordCharacters.concat(arrCharacters)
+    }
+    if (confirmNumericCharacter){
+        passwordCharacters = passwordCharacters.concat(arrNums)
+    }
+    if (confirmLowerCase){
+        passwordCharacters = passwordCharacters.concat(arrLowerCase)
+    }
+    if (confirmUpperCase){
+        passwordCharacters = passwordCharacters.concat(arrUpperCase)
+    }
+
+console.log(passwordCharacters)
+
+
+}
     
 
 
-
-    //var confirmLength <= 8 || confirmLength >=128
 //if outside parameters, start over
 
 //alert how many characters the password will have
@@ -56,4 +85,3 @@ function generatePassword(){
 
 //return pasword to #password
 
-//run
